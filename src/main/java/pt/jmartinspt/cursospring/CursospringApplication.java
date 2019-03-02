d3f50ac05a85f619fa60b2b1a49eebfad80ab1f9
Joel Martins <jmartins.com@gmail.com>
2019-03-02 14:21:17 +0000

    Operação de população da base de dados

--- src/main/java/pt/jmartinspt/cursospring/CursospringApplication.java
+++ src/main/java/pt/jmartinspt/cursospring/CursospringApplication.java
@@ -1,13 +1,34 @@
 package pt.jmartinspt.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pt.jmartinspt.cursospring.dao.CategoriaDao;
+import pt.jmartinspt.cursospring.domain.Categoria;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaDao categoriaDao;
 
     public static void main(String[] args) {
         SpringApplication.run(CursospringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Armazém");
+        Categoria cat4 = new Categoria(null, "Cozinha");
+
+        categoriaDao.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+
+    }
 }
