8c74391bb7e868d4d6cf2b79655e3dce33ba2520
Joel Martins <jmartins.com@gmail.com>
2019-03-02 12:49:49 +0000

    Rest testado

---
+++ src/main/java/pt/jmartinspt/cursospring/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package pt.jmartinspt.cursospring.resources;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+@RequestMapping(method = RequestMethod.GET)
+public String listar() {
+
+    return "Method ok";
+
+}
+
+
+
+}
